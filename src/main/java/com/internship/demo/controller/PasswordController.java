28168c7fb549d767ab0354e052969e2c4f2c3bcd
vinhnt <dongoc409@gmail.com>
2019-04-24 14:19:45 +0700

    update code

--- src/main/java/com/internship/demo/controller/PasswordController.java
+++ src/main/java/com/internship/demo/controller/PasswordController.java
@@ -1,6 +1,7 @@
 package com.internship.demo.controller;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,11 +14,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.internship.demo.dao.EmailDao;
 import com.internship.demo.dao.UsersDao;
 import com.internship.demo.domain.Users;
-import com.internship.demo.utils.StringUtils;
 
 @Controller
 @RequestMapping("/forgot")
@@ -42,32 +43,80 @@ public class PasswordController {
 			HttpServletRequest request) {
 
 		Optional<Users> optional = usersDao.findUserByMail(mail);
-
-		if (!optional.isPresent()) {
-			model.addAttribute("errorMessage", "Không tìm thấy địa chỉ email");
-		} else {
-
-			String randomPassword = StringUtils.generateRandomPassword(10);
+		
+		if (optional.isPresent()) {
 			Users user = optional.get();
+			user.setToken(UUID.randomUUID().toString());
+			usersDao.updateUser(user);
+
+			String appUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
 
 			// Email message
 			SimpleMailMessage passwordResetEmail = new SimpleMailMessage();
 			passwordResetEmail.setFrom("adminsystem@gmail.com");
 			passwordResetEmail.setTo(user.getMail());
 			passwordResetEmail.setSubject("Password Reset Request");
-			passwordResetEmail.setText("Mật khẩu mới: " + randomPassword);
+			passwordResetEmail.setText("Quên mật khẩu, click vào linh để đặt lại mật khẩu của bạn:\n" + appUrl
+					+ "/forgot/reset?token=" + user.getToken());
 
 			emailDao.sendEmail(passwordResetEmail);
-
-			user.setPassword(passwordEncoder.encode(randomPassword));
-			usersDao.changePasswordUser(user);
-
-			// Add success message to view
-			model.addAttribute("successMessage", "A password reset link has been sent to " + user.getMail());
 		}
+
+		model.addAttribute("successMessage", "Link đặt lại mật khẩu đã được gửi tới email " + mail);
 
 		return "forgot-password";
 
+	}
+
+	@GetMapping("/reset")
+	public String displayResetPasswordPage(Model model, @RequestParam("token") String token) {
+
+		Optional<Users> user = usersDao.findUserByToken(token);
+
+		if (user.isPresent()) {
+			model.addAttribute("token", token);
+		} else {
+			model.addAttribute("errorMessage", "Lỗi! Link reset mật khẩu không đúng.");
+			return "403";
+		}
+
+		return "reset-password";
+	}
+
+	@PostMapping("/reset")
+	public String setNewPassword(Model model, @RequestParam String token, @RequestParam String passwordNew,
+			@RequestParam("rePassword") String rePassword, RedirectAttributes redir) {
+
+		Optional<Users> user = usersDao.findUserByToken(token);
+
+		if (!passwordNew.equals(rePassword)) {
+			model.addAttribute("errorMessage", "Mật khẩu xác nhận không đúng");
+			return "reset-password";
+		}
+
+		if (passwordNew == null || rePassword == null) {
+			model.addAttribute("errorMessage", "Mật khẩu không được để trống");
+			return "reset-password";
+		} else {
+			if (user.isPresent()) {
+
+				Users resetUser = user.get();
+
+				resetUser.setPassword(passwordEncoder.encode(passwordNew));
+
+				resetUser.setToken(null);
+
+				usersDao.changePasswordUser(resetUser);
+
+				usersDao.updateUser(resetUser);
+
+				return "redirect:/login";
+
+			} else {
+				model.addAttribute("errorMessage", "Xảy ra lỗi");
+			}
+		}
+		return "reset-password";
 	}
 
 }
